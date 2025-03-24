eb0c14d83ec68505705a859ad91e5b567620cd67
Adheera24 <adheera.chil@gmail.com>
2025-03-23 21:34:31 -0400

    Update resources.swift

--- resources.swift
+++ resources.swift
@@ -4,7 +4,6 @@
 //
 //  Created by Adheera Chilamkurthi on 2/24/25.
 //
-
 import SwiftUI
 
 struct ResourcesView: View {
@@ -57,50 +56,11 @@ struct ResourcesView: View {
     }
 }
 
-struct ResourceSection: View {
-    let title: String
-    var website: String?
-    var phone: String?
-    var description: String?
-    
-    var body: some View {
-        VStack(alignment: .leading, spacing: 8) {
-            Text(title)
-                .font(.headline)
-                .foregroundColor(Color.purple)
-            
-            if let website = website {
-                Link(destination: URL(string: "https://\(website)")!) {
-                    Text(website)
-                        .foregroundColor(Color.blue)
-                }
-            }
-            
-            if let phone = phone {
-                Link(destination: URL(string: "tel:\(phone.filter { $0.isNumber })")!) {
-                    Text(phone)
-                        .foregroundColor(Color.orange)
-                }
-            }
-            
-            if let description = description {
-                Text(description)
-                    .foregroundColor(Color.pink)
-            }
-        }
-        .padding()
-        .background(
-            RoundedRectangle(cornerRadius: 12)
-                .fill(Color.white)
-                .shadow(color: Color.black.opacity(0.1), radius: 5)
-        )
+struct ResourcesView_Previews: PreviewProvider {
+    static var previews: some View {
+        ResourcesView()
     }
 }
 
-struct ResourcesView_Previews: PreviewProvider {
-    static var previews: some View {
-        NavigationView {
-            ResourcesView()
-        }
-    }
-}
+     
+              
